169c1311764fa563676a0529ce476469d07b1bc4
dibashthapa <dibashthapa55@gmail.com>
2020-02-03 15:54:43 +0545

    initial upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import *
+import csv
+import random
+app=Flask(__name__)
+
+Names=[]
+RandomNames=[]
+with open('file.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        Names.append(row[0])
+
+@app.route("/")
+def home():
+    random.shuffle(Names)
+    count=len(Names)
+    print(count)
+    return render_template("index.html",names=Names,count=count)
+if __name__=='__main__':
+    app.run(debug=True)
